f6e6d60edc19a441de532fa4718c5397f7abec51
Dustin Sallings <dustin@spy.net>
2008-06-22 12:22:46 -0700

    Allow sendmsg to pick up the beanstalk config from the environment.

--- sendmsg.rb
+++ sendmsg.rb
@@ -3,7 +3,17 @@
 require 'rubygems'
 require 'beanstalk-client'
 
-CONF = YAML.load_file 'jabber-bot.yml'
+begin
+  CONF = YAML.load_file 'jabber-bot.yml'
+rescue Errno::ENOENT
+  unless ENV.has_key?('BEANSTALK_SERVER') && ENV.has_key?('BEANSTALK_TUBE')
+    raise "No jabber-bot.yml or BEANSTALK_SERVER and BEANSTALK_TUBE env"
+  end
+  CONF = { 'beanstalk' => {
+    'server' => ENV['BEANSTALK_SERVER'],
+    'tube' => ENV['BEANSTALK_TUBE']
+    }}
+end
 
 BEANSTALK = Beanstalk::Pool.new [CONF['beanstalk']['server']]
 BEANSTALK.use CONF['beanstalk']['tube']
