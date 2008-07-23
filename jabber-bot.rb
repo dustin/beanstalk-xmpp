11eafb1bd86c06c09c52301735d34be62f8b274c
Dustin Sallings <dustin@spy.net>
2008-07-23 14:15:27 -0700

    Brought in convenience subclass for quicker responses.

--- jabber-bot.rb
+++ jabber-bot.rb
@@ -18,11 +18,6 @@ end
 
 CONF = YAML.load_file CONF_PATH
 
-JABBER = Jabber::Simple.new(CONF['xmpp']['jid'], CONF['xmpp']['pass'])
-JABBER.send!(Jabber::Presence.new(nil,
-  CONF['xmpp']['status'] || 'In service',
-  (CONF['xmpp']['priority'] || 1).to_i))
-
 BEANSTALK = Beanstalk::Pool.new [CONF['beanstalk']['server']]
 BEANSTALK.watch CONF['beanstalk']['tube']
 BEANSTALK.ignore 'default'
@@ -30,33 +25,59 @@ BEANSTALK.ignore 'default'
 TIMEOUT = CONF['general']['timeout'].to_i
 
 STATII = {}
+USER_STATUS = Hash.new :on
 
-def process_xmpp_incoming
-  JABBER.presence_updates { |user, status, message| STATII[user] = status }
-  JABBER.received_messages do |msg|
-    puts "<<< Received message from #{msg.from} #{msg.body}"
-    JABBER.deliver msg.from,
-      "Sorry, I don't understand you.  I'm just here to tell you about stuff."
-  end
-  JABBER.new_subscriptions { |from, presence| puts "Subscribed by #{from}" }
-  JABBER.subscription_requests { |from, presence| puts "Sub req from #{from}" }
+def process_xmpp_incoming(jabber)
+  jabber.presence_updates { |user, status, message| STATII[user] = status }
+  jabber.received_messages
+  jabber.new_subscriptions { |from, presence| puts "Subscribed by #{from}" }
+  jabber.subscription_requests { |from, presence| puts "Sub req from #{from}" }
 end
 
-def process_job(job)
+def process_job(jabber, job)
   to, msg = job.body.split(/\s+/, 2)
   msg = msg.strip
   puts "Got #{msg} for #{to}"
   if to == 'status'
-    JABBER.status nil, msg
+    jabber.status nil, msg
   else
     if STATII.has_key?(to) && ! [:dnd, :unavailable].include?(STATII[to])
       puts "Delivering message to #{to} #{STATII[to]}"
-      JABBER.deliver to, msg
+      jabber.deliver to, msg
     end
   end
 end
 
-def run_loop
+class MyClient < Jabber::Simple
+
+  def initialize(jid, pass)
+    super(jid, pass)
+    setup_callback
+  end
+    
+  def reconnect
+    puts "Reconnecting"
+    $stdout.flush
+    super
+    setup_callback
+  end
+
+  def setup_callback
+    client.add_message_callback do |msg|
+      begin
+        puts "<<< Received message from #{msg.from} #{msg.body}"
+        deliver msg.from,
+          "Sorry, I don't understand you.  I'm just here to tell you about stuff."
+      rescue StandardError, Interrupt
+        puts "Incoming message error:  #{$!}\n" + $!.backtrace.join("\n\t")
+        $stdout.flush
+        deliver message.from, "Error processing your message:  #{$!}"
+      end
+    end
+  end
+end
+
+def run_loop(jabber)
   job = begin
     puts "Waiting for a message at #{Time.now.to_s}"
     $stdout.flush
@@ -66,8 +87,8 @@ def run_loop
   end
   puts "Processing #{job} at #{Time.now.to_s}"
   $stdout.flush
-  process_xmpp_incoming
-  process_job job if job
+  process_xmpp_incoming jabber
+  process_job jabber, job if job
 rescue StandardError, Interrupt
   puts "Got exception:  #{$!.inspect}"
   sleep 5
@@ -79,6 +100,11 @@ ensure
   job.delete if job
 end
 
+jabber = MyClient.new(CONF['xmpp']['jid'], CONF['xmpp']['pass'])
+jabber.send!(Jabber::Presence.new(nil,
+  CONF['xmpp']['status'] || 'In service',
+  (CONF['xmpp']['priority'] || 1).to_i))
+
 loop do
-  run_loop
+  run_loop jabber
 end
