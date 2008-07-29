663912e602119988edc302ba2e494f5d2a66acfd
Dustin Sallings <dustin@spy.net>
2008-07-29 09:17:13 -0700

    Got rid of xmpp4r-simple.

--- jabber-bot.rb
+++ jabber-bot.rb
@@ -3,7 +3,8 @@
 require 'rubygems'
 require 'date'
 require 'set'
-require 'xmpp4r-simple'
+require 'xmpp4r'
+require 'xmpp4r/roster'
 require 'beanstalk-client'
 
 CONF_PATHS = [
@@ -25,23 +26,22 @@ BEANSTALK.ignore 'default'
 
 TIMEOUT = CONF['general']['timeout'].to_i
 
-class MyClient < Jabber::Simple
+class BotMain
 
   def initialize(jid, pass)
-    super(jid, pass)
-
     @statii = {}
     @user_status = Hash.new :on
     @ignoring = Hash.new {|h,k| h[k] = Set.new; h[k]}
 
-    setup_callback
-  end
-    
-  def reconnect
-    puts "Reconnecting"
-    $stdout.flush
-    super
-    setup_callback
+    @client = Jabber::Client.new(jid)
+    @client.connect
+    @client.auth(pass)
+
+    setup_callbacks
+
+    @client.send(Jabber::Presence.new(nil,
+      CONF['xmpp']['status'] || 'To Serve Man',
+      (CONF['xmpp']['priority'] || 1).to_i))
   end
 
   def willing_to_receive(jid, to)
@@ -64,11 +64,53 @@ class MyClient < Jabber::Simple
     end
   end
 
-  def process_xmpp_incoming
-    presence_updates { |user, status, message| @statii[user] = status }
-    received_messages
-    new_subscriptions { |from, presence| puts "Subscribed by #{from}" }
-    subscription_requests { |from, presence| puts "Sub req from #{from}" }
+  def subscribe_to(jid)
+    puts "Sending subscription request to #{jid}"
+    req = Jabber::Presence.new.set_type(:subscribe)
+    req.to = jid
+    @client.send req
+  end
+
+  def setup_callbacks
+
+    @client.on_exception do |e, stream, symbol|
+      puts "Exception in #{symbol}: #{e}" + e.backtrace.join("\n\t")
+      $stdout.flush
+    end
+
+    @roster = Jabber::Roster::Helper.new(@client)
+
+    @roster.add_subscription_request_callback do |roster_item, presence|
+      puts "Subscription requrest from #{presence.from.to_s}"
+      @roster.accept_subscription(presence.from)
+      subscribe_to presence.from.bare.to_s
+    end
+
+    @client.add_presence_callback do |presence|
+      status = presence.type.nil? ? :available : presence.type
+      @statii[presence.from.bare.to_s] = status
+      puts "*** #{presence.from} -> #{status}"
+      $stdout.flush
+    end
+
+    @client.add_message_callback do |msg|
+      begin
+        unless msg.body.nil?
+          puts "<<< Received message from #{msg.from} #{msg.body}"
+          cmd, args = msg.body.split /\s+/, 2
+          cmd_method = "cmd_#{cmd}".to_sym
+          if self.respond_to? cmd_method
+            self.send cmd_method, msg.from, args
+          else
+            deliver msg.from, "I don't understand #{cmd}"
+          end
+        end
+      rescue StandardError, Interrupt
+        puts "Incoming message error:  #{$!}\n" + $!.backtrace.join("\n\t")
+        $stdout.flush
+        deliver message.from, "Error processing your message:  #{$!}"
+      end
+    end
   end
 
   def cmd_ignore(from, group)
@@ -90,27 +132,21 @@ class MyClient < Jabber::Simple
     end
   end
 
-  def setup_callback
-    client.add_message_callback do |msg|
-      begin
-        unless msg.body.nil?
-          puts "<<< Received message from #{msg.from} #{msg.body}"
-          cmd, args = msg.body.split /\s+/, 2
-          cmd_method = "cmd_#{cmd}".to_sym
-          if self.respond_to? cmd_method
-            self.send cmd_method, msg.from, args
-          else
-            deliver msg.from, "I don't understand #{cmd}"
-          end
-        end
-      rescue StandardError, Interrupt
-        puts "Incoming message error:  #{$!}\n" + $!.backtrace.join("\n\t")
-        $stdout.flush
-        deliver message.from, "Error processing your message:  #{$!}"
-      end
+  def deliver(jid, message, type=:chat)
+    if message.kind_of?(Jabber::Message)
+      msg = message
+      msg.to = jid
+    else
+      msg = Jabber::Message.new(jid)
+      msg.type = type
+      msg.body = message
     end
+    @client.send msg
   end
+
 end
+
+# Jabber::debug = true
 
 def run_loop(jabber)
   job = begin
@@ -122,7 +158,6 @@ def run_loop(jabber)
   end
   puts "Processing #{job} at #{Time.now.to_s}"
   $stdout.flush
-  jabber.process_xmpp_incoming
   jabber.process_job job if job
 rescue StandardError, Interrupt
   puts "Got exception:  #{$!.inspect}"
@@ -135,10 +170,7 @@ ensure
   job.delete if job
 end
 
-jabber = MyClient.new(CONF['xmpp']['jid'], CONF['xmpp']['pass'])
-jabber.send!(Jabber::Presence.new(nil,
-  CONF['xmpp']['status'] || 'In service',
-  (CONF['xmpp']['priority'] || 1).to_i))
+jabber = BotMain.new(CONF['xmpp']['jid'], CONF['xmpp']['pass'])
 
 loop do
   run_loop jabber
